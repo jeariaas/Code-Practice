ce674cda4aa488f5e8609b3cfa5c7ed40784f125
Jesus Arias <jeariaas@gmail.com>
2024-05-09 10:21:49 -0700

    Understanding the Visible Tree Nodes

--- Depth First Search/Visible Tree Nodes.py
+++ Depth First Search/Visible Tree Nodes.py
@@ -12,11 +12,12 @@ def visible_tree_node(root: Node) -> int:
         total = 0
         
         if root.val >= max_value:
-            total +=1
+            total += 1
+        
         total += dfs(root.left, max(max_value, root.val))
         total += dfs(root.right, max(max_value, root.val))
         return total
-    
+
     return dfs(root, -float('inf'))
 
 # this function builds a tree from input; you don't have to modify it
