301040470ba5525b745af1f0809826843567565a
Jesus Arias <jeariaas@gmail.com>
2024-05-13 09:50:57 -0700

    Update Balanced Binary Tree.py

--- Depth First Search/Balanced Binary Tree.py
+++ Depth First Search/Balanced Binary Tree.py
@@ -1,20 +1,30 @@
+
 class Node:
     def __init__(self, val, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
+
 def tree_height(tree):
+    # WRITE YOUR BRILLIANT CODE HERE
     if not tree:
         return 0
+    
     left_height = tree_height(tree.left)
     right_height = tree_height(tree.right)
 
     if left_height == -1 or right_height == -1:
         return -1
+    
     if abs(left_height - right_height) > 1:
         return -1
+    
     return max(left_height, right_height) + 1
+    
+
+
+
 
 def is_balanced(tree: Node) -> bool:
     # WRITE YOUR BRILLIANT CODE HERE
@@ -29,7 +39,10 @@ def build_tree(nodes, f):
     right = build_tree(nodes, f)
     return Node(f(val), left, right)
 
+
+
 if __name__ == '__main__':
+
     root = build_tree(iter(input().split()), int)
     res = is_balanced(root)
-    print(res)
+    print('true' if res else 'false')
